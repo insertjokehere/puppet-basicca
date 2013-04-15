4da65fbfa46c4baf4795d513330f148728f3c1fe
William Hughes <whlabratz@gmail.com>
2013-04-16 10:12:16 +1200

    fixed string concat errors

--- manifests/cert.pp
+++ manifests/cert.pp
@@ -1,12 +1,12 @@
 define basicca::cert($storedir="/var/ssl", $subject) {
 
 	basicca::privatekey { "${name}-private":
-		saveto => "{$storedir}/${name}.key",
+		saveto => "${storedir}/${name}.key",
 	}
 
 	basicca::csr { "${name}-csr":
-		saveto => "{$storedir}/${name}.csr",
-		key	   => "{$storedir}/${name}.key",
+		saveto => "${storedir}/${name}.csr",
+		key	   => "${storedir}/${name}.key",
 		subject => $subject,
 		require => Basicca::Privatekey["${name}-private"],
 	}
