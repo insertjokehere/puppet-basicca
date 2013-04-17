46fd71752ac2c7fc8a2a04dc74b0085c9daa347d
William Hughes <whlabratz@gmail.com>
2013-04-17 12:56:55 +1200

    added certrequest convienence define

---
+++ manifests/certrequest.pp
@@ -0,0 +1,20 @@
+define basicca::certrequest($keypath, $csrpath, $subject=undef, $config=undef, $keysize=2048, $owner="root", $group="root") {
+
+	basicca::privatekey { "${name}-private":
+		saveto  => $keypath,
+		owner   => $owner,
+		group   => $group,
+		numbits => $keysize,
+	}
+
+	basicca::csr { "${name}-csr":
+		saveto  => $csrpath,
+		key	    => $keypath,
+		subject => $subject,
+		config  => $config,
+		owner   => $owner,
+		group   => $group,
+		require => Basicca::Privatekey["${name}-private"],
+	}
+
+}
