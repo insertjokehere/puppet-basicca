a07dab067a5ab7e24abe8d4a4a501fbf5d3b6681
William Hughes <whlabratz@gmail.com>
2013-04-21 11:05:37 +1200

    adding support for subjectAltNames

--- manifests/csr.pp
+++ manifests/csr.pp
@@ -1,11 +1,27 @@
-define basicca::csr($subject=undef, $key, $saveto, $owner="root", $group="root", $mode="0600", $config=undef) {
+define basicca::csr($subject=undef, $key, $saveto, $owner="root", $group="root", $mode="0600", $config=undef, $altNames=undef, $altNamesEnv="ALTNAME") {
 
 	if ($subject == undef and $config == undef) {
 		fail("Must specify one of subject or config")
 	}
 
+	if ($altNames != undef and $config == undef) {
+		fail("Must specify a config file for altNames to work")
+	}
+
 	if ($config != undef) {
 		$config_cmd = "-config ${config}"
+	}
+
+	if ($altNames != undef) {
+		if(is_array($altNames)) {
+			# ["a","b"] => "a,DNS:b" => "DNS:a,DNS:b"
+			$st = join($altNames,",DNS:")
+			$altNameStr = "DNS:${st}"
+		} else {
+			$altNameStr=$altNames
+		}
+	} else {
+		$altNameStr=""
 	}
 
 	if ($subject != undef) {
@@ -23,6 +39,7 @@ define basicca::csr($subject=undef, $key, $saveto, $owner="root", $group="root",
 	exec { $name:
 		command => "/usr/bin/openssl req -new -key ${key} -out ${saveto} ${subject_cmd} ${config_cmd}",
 		creates => $saveto,
+		environment => "${altNamesEnv}=$altNameStr",
 	}
 
 	file { $saveto:
