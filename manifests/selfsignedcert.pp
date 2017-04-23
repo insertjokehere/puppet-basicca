edffb07ae7bf6e4f5428ce3c167193fc49a8f092
William Hughes <williamh@catalyst.net.nz>
2017-04-24 09:12:47 +1200

    Update to allow overriding the signing algo for certs

--- manifests/selfsignedcert.pp
+++ manifests/selfsignedcert.pp
@@ -1,31 +1,32 @@
-define basicca::selfsignedcert($subject=undef, $config=undef, $keypath, $csrpath, $certpath, $owner="root", $group="root", $keysize=4096, $certmode="0644", $issuelength=365) {
+define basicca::selfsignedcert($subject=undef, $config=undef, $keypath, $csrpath, $certpath, $owner="root", $group="root", $keysize=4096, $certmode="0644", $issuelength=365, $sign_algo='sha256') {
 
-	basicca::privatekey { "${name}-private":
-		saveto  => $keypath,
-		owner   => $owner,
-		group   => $group,
-		numbits => $keysize,
-	}
+  basicca::privatekey { "${name}-private":
+    saveto  => $keypath,
+    owner   => $owner,
+    group   => $group,
+    numbits => $keysize,
+  }
 
-	basicca::csr { "${name}-csr":
-		saveto  => $csrpath,
-		key	    => $keypath,
-		subject => $subject,
-		config  => $config,
-		owner   => $owner,
-		group   => $group,
-		require => Basicca::Privatekey["${name}-private"],
-	}
+  basicca::csr { "${name}-csr":
+    saveto  => $csrpath,
+    key     => $keypath,
+    subject => $subject,
+    config  => $config,
+    owner   => $owner,
+    group   => $group,
+    require => Basicca::Privatekey["${name}-private"],
+  }
 
-	basicca::signcert { "${name}-cert":
-		signkey => $keypath,
-		csr     => $csrpath,
-		saveto  => $certpath,
-		days    => $issuelength,
-		owner   => $owner,
-		group   => $group,
-		mode    => $certmode,
-		require => Basicca::Csr["${name}-csr"],
-	}
+  basicca::signcert { "${name}-cert":
+    signkey   => $keypath,
+    csr       => $csrpath,
+    saveto    => $certpath,
+    days      => $issuelength,
+    owner     => $owner,
+    group     => $group,
+    mode      => $certmode,
+    sign_algo => $sign_algo,
+    require   => Basicca::Csr["${name}-csr"],
+  }
 
 }
--- manifests/signcert.pp
+++ manifests/signcert.pp
@@ -1,16 +1,15 @@
-define basicca::signcert($signkey, $csr, $saveto, $days=365, $owner="root", $group="root", $mode="0600") {
+define basicca::signcert($signkey, $csr, $saveto, $days=365, $owner="root", $group="root", $mode="0600", $sign_algo='sha256') {
 
-	exec { "${name}-cert":
-		command => "/usr/bin/openssl x509 -req -sha256 -days ${days} -in ${csr} -signkey ${signkey} -out ${saveto}",
-		creates => $saveto
-	}
+  exec { "${name}-cert":
+    command => "/usr/bin/openssl x509 -req -${sign_algo} -days ${days} -in ${csr} -signkey ${signkey} -out ${saveto}",
+    creates => $saveto
+  }
 
-	file { $saveto:
-		ensure  => file,
-		owner   => $owner,
-		group   => $group,
-		mode    => $mode,
-		require => Exec["${name}-cert"],
-	}
-
+  file { $saveto:
+    ensure  => file,
+    owner   => $owner,
+    group   => $group,
+    mode    => $mode,
+    require => Exec["${name}-cert"],
+  }
 }
